540aecbacbe7cf0c13a8f4731b45af3114df5dc9
augus.zou <augus.zou@bindo.com>
2017-10-09 01:46:28 +0800

    fix ssh

--- scripts/slave.sh
+++ scripts/slave.sh
@@ -15,16 +15,15 @@ export slave_mysql_root_passwd="123"
 export slave_ssh_login_user="ubuntu"
 export slave_ssh_login_passwd="vagrant"
 
-cmd_ssh="sshpass -p ${master_ssh_login_passwd} ssh ${master_ssh_login_user}@${master_ip}"
+cmd_ssh="sshpass -p ${master_ssh_login_passwd} ssh -o StrictHostKeyChecking=no -o CheckHostIP=no -o UserKnownHostsFile=/dev/null ${master_ssh_login_user}@${master_ip}"
 cmd_status="mysql -uroot -p${master_mysql_root_passwd} -e 'show master status\G'"
 export status=`${cmd_ssh} '${cmd_status}'`
 export binlogname=`echo \"$status\" | grep \"File\" | awk '{print $2}'`
-export postion=`echo "$status" | grep "Position" | awk '{print $2}'`
+export position=`echo "$status" | grep "Position" | awk '{print $2}'`
 
 mysql -uroot -p${slave_mysql_root_passwd} -e "drop database if exists ${replication_db};create database ${replication_db};"
 mysql -uroot -p${slave_mysql_root_passwd} ${replication_db} < /vagrant/${replication_db}.sql
 
-# sed -i 's/^server-id.*$/server-id = ${slave_server_id}/g' /etc/my.cnf
 cat >> /etc/mysql/mysql.conf.d/mysqld.cnf <<EOF
 server-id=${slave_server_id}
 replicate-do-db=test
