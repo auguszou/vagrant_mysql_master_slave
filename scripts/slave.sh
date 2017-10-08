f9a2e4e498fd39ad5e18ea8c4a6c9a64ffd32c74
augus.zou <augus.zou@bindo.com>
2017-10-08 22:00:31 +0800

    fix slave

--- scripts/slave.sh
+++ scripts/slave.sh
@@ -16,14 +16,10 @@ export slave_ssh_login_user="vagrant"
 export slave_ssh_login_passwd="vagrant"
 
 cmd_ssh="sshpass -p ${master_ssh_login_passwd} ssh ${master_ssh_login_user}@${master_ip}"
-cmd_status="${master_}${mysqlbinpath}/mysql -uroot -p${master_mysql_root_passwd} -e \"show master status\G\""
+cmd_status="mysql -uroot -p${master_mysql_root_passwd} -e \"show master status\G\""
 export status=`${cmd_ssh} -e '${cmd_status}'`
-
-cmd_binlogname="echo \"$status\" | grep \"File\" | awk '{print $2}'"
-export binlogname=`${cmd_ssh} -e '${cmd_binlogname}'`
-
-cmd_postion="echo "$status" | grep "Position" | awk '{print $2}'"
-export position=`${cmd_ssh} -e '${cmd_postion}'`
+export binlogname=`echo \"$status\" | grep \"File\" | awk '{print $2}'`
+export postion=`echo "$status" | grep "Position" | awk '{print $2}'1
 
 mysql -uroot -p${slave_mysql_root_passwd} -e "drop database if exists ${replication_db};create database ${replication_db};"
 mysql -uroot -p${slave_mysql_root_passwd} ${replication_db} < /vagrant/${replication_db}.sql
@@ -37,11 +33,11 @@ EOF
 
 mysql -uroot -p${slave_mysql_root_passwd} <<EOF
 STOP SLAVE;
-CHANGE MASTER TO MASTER_HOST=\"${master_ip}\",
-MASTER_USER=\"${replication_user}\",
-MASTER_PASSWORD=\"${replication_passwd}\",
+CHANGE MASTER TO MASTER_HOST="${master_ip}",
+MASTER_USER="${replication_user}",
+MASTER_PASSWORD="${replication_passwd}",
 MASTER_PORT=3306,
-MASTER_LOG_FILE=\"${binlogname}\",
+MASTER_LOG_FILE="${binlogname}",
 MASTER_LOG_POS=${position},
 MASTER_CONNECT_RETRY=10;
 START SLAVE;
